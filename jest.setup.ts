38b8152400c25725b61934b8f7cb8f671aa2ec36
Roman Dziuba <roman.dziuba@oneplusone.solutions>
2025-07-31 21:37:51 +0300

    fixes

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,6 +1,9 @@
 import '@testing-library/jest-dom';
 import 'cross-fetch/polyfill';
 global.ResizeObserver = class {
+  private callback: ResizeObserverCallback;
+
+
   constructor(callback: any) {
     this.callback = callback;
   }
